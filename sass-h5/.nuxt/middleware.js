a36f0c31fab0a66456c8f2de589febccce1fccf7
张英美 <1935686874@qq.com>
2021-06-10 17:25:25 +0800

    fix “去掉.nuxt”

--- sass-h5/.nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['auth'] = require('../middleware/auth.js')
-middleware['auth'] = middleware['auth'].default || middleware['auth']
-
-middleware['noauth'] = require('../middleware/noauth.js')
-middleware['noauth'] = middleware['noauth'].default || middleware['noauth']
-
-export default middleware
